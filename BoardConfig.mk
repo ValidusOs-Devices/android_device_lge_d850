0e07acaefdccd06bee54965418f66701b79f5625
Matthew Opperman <mattopp17@gmail.com>
2016-03-08 02:37:44 -0500

    try 5.3

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,11 +29,11 @@ TARGET_KERNEL_CONFIG := cyanogenmod_d850_defconfig
 TARGET_REQUIRES_BUMP := true
 
 # Kernel Toolchain
- KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+ KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.3-uber/bin
  KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
  
 # Rom Toolchain
- TARGET_GCC_VERSION_EXP := 4.9-uber
+ TARGET_GCC_VERSION_EXP := 5.3-uber
  
 # Optimizations
  CLANG_O3 := false
