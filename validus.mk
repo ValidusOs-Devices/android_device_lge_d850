9303c03b9502ed209330667ac19b364ecb9a8095
Adam <ovadia.adam@gmail.com>
2016-12-23 21:17:33 -0500

    add some lg magic

--- validus.mk
+++ validus.mk
@@ -9,6 +9,7 @@ $(call inherit-product, vendor/validus/config/nfc_enhanced.mk)
 PRODUCT_NAME := validus_d850
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Adam Ovadia (oadam11)" \
     PRODUCT_DEVICE="g3" \
     PRODUCT_NAME="g3_att_us" \
     BUILD_FINGERPRINT="lge/g3_att_us/g3:4.4.2/KVT49L.D85010d/D85010d.1403173065:user/release-keys" \
